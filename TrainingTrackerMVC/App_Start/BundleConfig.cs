7ce1d76a000b4ef4549ca0755a39b33b7d71476e
Andy Cornforth <andycornforth@live.co.uk>
2016-06-22 17:39:44 +0100

    added a default css file to add general styles

--- TrainingTrackerMVC/App_Start/BundleConfig.cs
+++ TrainingTrackerMVC/App_Start/BundleConfig.cs
@@ -23,9 +23,8 @@ namespace TrainingTrackerMVC
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/default.css"));
         }
     }
 }
